8642b4778a3cbb1ed1b41131ddb3a6c412f3bb5a
Raúl Arcos Herrera <raularcosherrera@gmail.com>
2022-09-13 12:37:12 +0200

    Versión Final Ejercicio 1

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php 
-//Incluimos el header (Configuración de db y carga de css).
 include('assets/header/prueba-yunbit-header.php');
 
-//Seleccionamos nombre, dirección y teléfono de todos los clientes en la db.
 $client = $connection->prepare("SELECT NAME,ADDRESS,TELF FROM test_clients");
 $client->execute();
 
@@ -10,21 +8,19 @@ $client->execute();
 <body>
 
 <div class="container">
-      <div class="row">
-        <!-- Columna Izquierda de Clientes -->
-        <div class="col-lg-12 mb-3">
-          <!-- Cliente individual -->
-          <?php foreach ($client as $c){ ?>
-            <div class="card  pos-relative py-3 px-3 mb-3 border-warning">
-                <div class="row align-items-center">
-                <div class="col-md-8 mb-3 mb-sm-0">
-                    <h5> <?php echo $c['NAME'] ?></h5>
-                    <p class="text-sm"><?php echo $c['ADDRESS'] ?></p>
-                    <p class="text-sm"><?php echo $c['TELF'] ?></p>
+    <div class="row">
+        <div class="col-md-12">
+            <?php foreach ($client as $c){ ?>
+                <div class="card pos-relative py-3 px-3 mb-3 border-warning">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <h5> <?php echo $c['NAME'] ?></h5>
+                            <p class="text-sm"><?php echo $c['ADDRESS'] ?></p>
+                            <p class="text-sm"><?php echo $c['TELF'] ?></p>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-          <?php } ?>
+            <?php } ?>
         </div>
     </div>
 </body>
