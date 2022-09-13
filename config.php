cf36a7958a7a0e1757f7298a3486d9d9899cc787
Raúl Arcos Herrera <raularcosherrera@gmail.com>
2022-09-13 15:17:52 +0200

    Versión Final

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
-//config.php para que, en caso de tener que utilizar la bd en otros archivos no repetir código.
-//Definimos los datos de nuestra base de datos.
+
 define('USER', 'pruebajr');
 define('PASSWORD', 'pruebajr');
 define('HOST', 'localhost');
 define('DATABASE', 'pruebas_practicas');
  
-//Realizamos la conexión, en caso de error se devuelve una excepción.
 try {
     $pdo = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
 } catch (PDOException $e) {
